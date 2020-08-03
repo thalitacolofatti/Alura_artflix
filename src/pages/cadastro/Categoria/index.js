ab4046a4d91f6d2d526a9c7ccd7139a555050679
Thalita Colofatti <thalitacolofatti@gmail.com>
2020-08-02 23:47:50 -0300

    update url heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,8 +29,9 @@ function CadastroCategoria() {
     }
 
     useEffect(() => {
-        console.log('alo alo');
-        const URL_TOP = 'http://localhost:8080/categorias'
+        const URL_TOP = window.location.hostname.includes('localhost')
+        ? 'http://localhost:8080/categorias'
+        : 'https://alura-artflix.herokuapp.com/categorias'
         fetch(URL_TOP)
             .then (async (respostaDoServidor) => {
                 const resposta = await respostaDoServidor.json();
